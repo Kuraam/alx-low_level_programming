ea3e4733fecf322fcb834a2223551af8b46e9cee
Ahmad <kuraahmad050@gmail.com>
2023-02-17 18:44:21 -0800

    hfhf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - 0-9
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int i = 1;
+
+while (i < 10)
+{
+printf("%d",i);
+i++;
+}
+putchar('\n');
+return (0);
+}
+
