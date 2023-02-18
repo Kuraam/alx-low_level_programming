145592be1e184b21f66f8af0afb6d52c0ad9d212
Ahmad <kuraahmad050@gmail.com>
2023-02-17 17:51:27 -0800

    hf

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Alphabets except q and e
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char c = 'a';
+
+do {
+
+if (c == 'p' || c == 'e') 
+{
+/* skip the iteration */
+c++;
+continue;
+}
+putchar(c);
+c++;
+}
+while (c <= 'z');
+putchar('\n');
+return (0);    
+}
