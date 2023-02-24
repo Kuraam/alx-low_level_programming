725beea9c5c55e50a38febc4c982ca3f6153ab2c
Ahmad <kuraahmad050@gmail.com>
2023-02-24 06:29:54 -0800

    sjs

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_last_digit - prints the sign of a number.
+* @i: character to test.
+* Return: Always 0.
+*/
+int print_last_digit(int i)
+{
+int m;
+
+m = (i % 10);
+return (m);
+}
