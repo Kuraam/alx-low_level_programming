dc22d21980f99fdda74f3781bd8c5faf71917781
Ahmad <kuraahmad050@gmail.com>
2023-03-15 11:27:01 -0700

    uu

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,17 +25,17 @@ j++;
 j = 0;
 i++;
 }
-s = malloc((sizeof(char) * ch) + ac + 1);
+s = malloc((sizeof(char) * ch) +ac + 1);
 i = 0;
 while (av[i])
 {
-while(av[i][j])
+while (av[i][j])
 {
 s[k] = av[i][j];
 k++;
 j++;
 }
-s[k] ='\n';
+s[k] = '\n';
 j = 0;
 k++;
 i++;
